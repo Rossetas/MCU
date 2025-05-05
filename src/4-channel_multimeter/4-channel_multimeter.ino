5ac131f8ddd72da8dd04981ace7412b01c252ce6
Marco Vasquez Ovares <markos.vasquez1993@gmail.com>
2025-05-04 18:28:28 -0600

    primer checkpoint, diseño listo

---
+++ src/4-channel_multimeter/4-channel_multimeter.ino
@@ -0,0 +1,14 @@
+// Marco Vasquez Ovares
+// B17032
+
+#include <PCD8544.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
